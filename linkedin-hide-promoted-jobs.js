3a5e1280117d4dd87c82dd811e0f1a7b157a9964
westonh2 <weston@full-circle.design>
2023-07-02 07:48:24 -0400

    Create linkedin-hide-promoted-jobs.js

---
+++ linkedin-hide-promoted-jobs.js
@@ -0,0 +1,29 @@
+javascript: (() => {
+
+    console.log(document.querySelectorAll('.job-card-list__footer-wrapper'));
+    const jobList = document.querySelector('.jobs-search-results-list');
+
+    jobList.addEventListener('scroll', removePromoted);
+
+    jobList.scrollTo({top: jobList.scrollHeight, left: 0, behavior: "smooth"});
+
+    function removePromoted() {
+        if (jobList.scrollTop >= jobList.scrollHeight - jobList.offsetHeight*2) {
+            jobList.scrollTo({top: 0, left: 0, behavior: "smooth"});
+            setTimeout(() => {
+                const jobsElements = document.querySelectorAll('.job-card-list__footer-wrapper');
+                console.log(jobsElements);
+
+                jobsElements.forEach(job => {
+                    if (job.children[0].innerHTML.includes('Promoted')) {
+                        console.log('this one is promoted!');
+                        /* job.parentElement.parentElement.parentElement.style.backgroundColor = 'lightgray'; */
+                        job.parentElement.parentElement.parentElement.style.display = 'none';
+                    }
+                });
+                
+            }, 1000);
+            jobList.removeEventListener('scroll', removePromoted);
+        }
+    }
+})();
